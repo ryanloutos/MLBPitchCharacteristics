3c4922147112ca906eab3d67111ee49c75d372f1
Mitchell Black <mitchell.black@wustl.edu>
2020-11-23 12:02:20 -0600

    Added pitcher selector box
    Pitcher selector box populates with % similarity under a given
    threshold as a metric

--- milestone-1/js/main.js
+++ milestone-1/js/main.js
@@ -1,6 +1,10 @@
 setup_profile_input();
 
 function setup_profile_input(){
+    // Default readings
+    update_bauer_units()
+    update_movement_profile()
+
     $("#h-movement-slider").on("input", function(event){
         $("#h-movement-label").text(event.target.value)
         update_movement_profile()
@@ -45,8 +49,45 @@ function setup_profile_input(){
     }
 }
 
-function get_pitchers(){
+function get_pitchers(h_move, v_move, rpm, velo, b_units){
+    $.getJSON("/milestone-1/data/pitcher_data.json", function(data){
 
+        $("#pitchers-select-list").empty();
+        data.forEach(pitcher => {
+            // console.log(pitcher)
+            let name = pitcher.name
+            let fb = null;
+            // get primary fastball pitch
+            if(pitcher.pitches.FF.velocity != 0){
+                fb = pitcher.pitches.FF
+            } else if(pitcher.pitches.SI.velocity != 0){
+                fb = pitcher.pitches.SI
+            } else if(pitcher.pitches.FC.velocity != 0){
+                fb = pitcher.pitches.FC
+            } else {
+                // skips over the rest of loop for this pitcher
+                return;
+            }
+
+            // base similarity metric
+            // if percent difference in all categories is below a threshold add it
+            // to the selectable list of pitchers
+            let threshold = 0.1;
+
+            if (get_pct_threshold(h_move, fb.hor_movement, threshold) &&
+                get_pct_threshold(v_move, fb.vert_movement, threshold) &&
+                get_pct_threshold(b_units, fb.bauer_units, threshold)){
+                    console.log(pitcher)
+                    $("#pitchers-select-list").append(`<li>${name}</li>`);
+                }
+
+
+        });
+    })
+
+    function get_pct_threshold(amatuer, mlb, threshold){
+        return ((amatuer - mlb) / mlb) > threshold ? false : true ;
+    }
 }
 
 function get_data(){
